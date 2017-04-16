341e239d19db243af7a18fef310636edd8ec1c27
mhadji <majidhadji@gmail.com>
2017-04-16 09:38:28 -0700

    lab3 added

---
+++ lab3.sh
@@ -0,0 +1,33 @@
+#Authot = majid hadji , 4/13/2017 , lab3 cs225 edmonds CC
+#Check a users ID and if they're root then display a message saying it's insecure to run this script as root. If they are not root then get their home directory from the /etc/password file and display the size of the contents of it. 
+ 
+#!/bin/bash
+
+#Get the users ID exit if it is root 
+if [[ EUID -eq 0 ]]; then
+   echo "You cannot run this script as root"
+   exit
+else
+#If the user is not root make sure their userid is larger than 500
+   if [[ EUID -gt 500 ]]; then
+#Check to see if /etc/password exists and is readable
+      if [ ! -e /etc/passwd ] || [ ! -r /etc/passwd ] ; then
+        echo "/etc/passwd not found or it is not readable!!! "
+      else
+#Get the users home directory from /etc/passwd
+        userhome=$(awk -v uid=$UID -F":" '{ if($3==uid){print $6} }' /etc/passwd)
+        echo "user home directory is $userhome" 
+#Display the size of their home directory
+        userhomesize=$(du --max-depth=0  /home/majid/ | cut -f1 )        
+        echo "user home directory size is $userhomesize kb " 
+      fi 
+   else 
+      echo "You cannot run this script with UID = $UID "
+      exit
+ 
+   fi
+
+   
+fi
+  
+#Display the size of their home directory
---
+++ temp.sh
@@ -0,0 +1,13 @@
+#Authot = majid hadji , 4/9/2017 , lab1 cs225 edmonds CC
+#Prompt the user for a number and check to see if it's larger than 5
+ 
+#!/bin/bash
+x=1;
+ clear
+echo -e "This program check to see if the entered number is larger than 5."
+	
+while [ $x -gt 0 ]
+do
+        
+done
+
