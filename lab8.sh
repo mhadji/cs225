62915f9a74d629ce8fd205b72bcb0fba29027ace
mhadji <majidhadji@gmail.com>
2017-04-30 19:15:30 -0700

    commenting on lab8

--- lab8.sh
+++ lab8.sh
@@ -2,23 +2,18 @@
 #using the last line of /var/share/CS225/addresses.csv gather each field into the following variables using bash pattern substitution.  Echo the results back onto the screen all in lower case. Do NOT use awk or tr for this!
 
 #!/bin/bash
-
 LIST=$(cat addresses.csv)
-
-
 unset lastline
-
-
+#reading last line
 while read line; do
        lastline=$line
 done <<< "$LIST"
-
-     lastline=${lastline//'"'/} 
+# erasing "
+lastline=${lastline//'"'/} 
 
 OLDIFS=$IFS
-#IFS=" , "
+# reading last line and assigining values to variables 
 while IFS=,  read  FNAME  LNAME COMPANY STREET CITY  STATE  STATEABR ZIP HPHONE WPHONE  EMAIL WEBURL
-
  do
    echo -e "FNAME:   ${FNAME,,}\n\
 LNAME:   ${LNAME,,}\n\
@@ -32,7 +27,6 @@ HPHONE:  $HPHONE\n\
 WPHONE:  $WPHONE\n\
 EMAIL:   ${EMAIL,,}\n\
 WEBURL:  ${WEBURL,,}\n"
-
  done <<< $lastline
 IFS=$OLDIFS
 
