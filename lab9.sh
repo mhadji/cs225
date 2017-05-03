d60c9b49831d48fd40f118eb5241a363977a5811
mhadji <majidhadji@gmail.com>
2017-05-03 10:10:30 -0700

    lab9 in progress

--- lab9.sh
+++ lab9.sh
@@ -12,13 +12,38 @@
 # 3-Puts all additional arguments in $1 $2 etc
 
 #!/bin/bash
-while [ ‐n "$1" ]
-do
-   case "$1" in
-   ‐a) echo "Found the ‐a option" ;;
-   ‐b) echo "Found the ‐b option";;
-   ‐c) echo "Found the ‐c option" ;;
+# if no command line arg given
+# set rental to Unknown
+if [ -z $1 ]; then
+  echo "Script take argument.Here is some help"
+  arg=-h
+elif [ -n $1 ]
+then
+# otherwise make first arg as a rental
+  arg=$1
+fi
+
+   case "$arg" in
+   -d) echo "Option -d turns on debugging" 
+    # -d - turns on debugging (set -x)
+       set -x;;
+   -v) echo "option -v sets the VERBOSE variable to true"
+   #-v -sets the VERBOSE variable to true
+       set -v;;
+   -h) echo "Script name - $0"
+   #-h - prints out a help message with the name of the script (and no path information) plus the options
+        echo -e "the options:\n\
+        -d - turns on debugging
+        -v - sets the VERBOSE variable to true
+        -h - prints out a help message with the name of the script (and no path information) plus the options
+        -n <name> - takes a name argument and prints a message on the screen
+        -l <dir> - have it list the directory using a for loop or ls (ask yes/no before proceeding)";;
+   -n)  echo "Heloo $2";;
+   #-n <name> - takes a name argument and prints a message on the screen
+        
+   -l) echo "option -l "
+   # -l <dir> - have it list the directory using a for loop or ls (ask yes/no before proceeding)
+  ls "$2" ;;
    *) echo "$1 is not an option";;
    esac
-   shift
-done
+ 
