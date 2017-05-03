bcdfaed18a28fb3c558f2d03fb95af927bf45514
mhadji <majidhadji@gmail.com>
2017-05-03 15:46:33 -0700

    lab8xc final

---
+++ lab8xc.sh
@@ -0,0 +1,8 @@
+#Authot = majid hadji , 5/3/201 , lab8-Extra cs225 edmonds CC
+#Make a shell script that will take two arguments and complain and exit 
+#if the first doesn't exist and set a default value of 10 if the second doesn't exist.
+
+#!/bin/bash
+ echo  "The first argument is ${1:?"You need at least one argument."} and the Second argument is ${2:-10}." 
+
+
